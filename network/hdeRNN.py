5a8bd166e3205ff1f5fd606b4db13212a1821ece
tungnx94 <tungnx94@gmail.com>
2019-04-10 00:42:37 +0200

    minor update

--- network/hdeRNN.py
+++ network/hdeRNN.py
@@ -5,21 +5,28 @@ from .hdeReg import HDEReg
 
 class HDE_RNN(HDEReg):
 
-    def __init__(self, extractor, hidNum=256, rnnHidNum=1024, device=None):
-        print("SIMPLE RNN")
+    def __init__(self, extractor, hidNum=256, rnnHidNum=128, device=None):
         self.hidNum = hidNum
         self.rnnHidNum = rnnHidNum
 
         HDEReg.__init__(self, extractor, hidNum, device, init=False)
 
+        """
+        self.f2i = nn.Sequential(
+            nn.Linear(fNum, hidNum),
+            nn.ReLU()
+        )
+        """
+
         self.i2h = nn.Sequential(
-            nn.Linear(hidNum + rnnHidNum, rnnHidNum)
-            #nn.ReLU()
+            nn.Linear(hidNum + rnnHidNum, rnnHidNum),
+            nn.ReLU()
         )
         self.i2o = nn.Sequential(
-            nn.Linear(hidNum + rnnHidNum, hidNum)
-            #nn.ReLU()
+            nn.Linear(hidNum + rnnHidNum, hidNum),
+            nn.ReLU()
         )
+
         self.reg = nn.Sequential(
             nn.Linear(hidNum, 64),
             nn.ReLU(),
@@ -39,10 +46,15 @@ class HDE_RNN(HDEReg):
 
     def forward(self, x):
         seq_length = x.shape[0]
-        #print(seq_length)
         x = x.to(self.device)
         x = self.feature(x).squeeze()
 
+        """
+        x = self.f2i(x)
+        x = self.reg(x)
+        return x
+        """
+        
         hidden = torch.zeros(self.rnnHidNum).to(self.device)
         outputs = []
 
@@ -50,4 +62,5 @@ class HDE_RNN(HDEReg):
             out, hidden = self.forward_one_step(x[i], hidden)
             outputs.append(out)
 
-        return torch.stack(outputs)
+        outputs = torch.stack(outputs)
+        return outputs
